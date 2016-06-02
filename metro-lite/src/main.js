ef7d33487e621697a2f424ab49d72b960f647e89
Frederik Hanghøj Iversen <frhi@netcompany.dk>
2016-06-02 15:34:33 +0200

    Implement logic to change bpm and signature

--- metro-lite/src/main.js
+++ metro-lite/src/main.js
@@ -1,26 +1,55 @@
 this.metronome = (function() {
     var prev  = performance.now(),
-        delta = 800,
         tick  = new Audio('assets/Blip.ogg')
         step  = function() {
             tick.play();
         },
         isLooping = false;
-    function loop() {
+    function loop(cfg) {
         if(!isLooping) return;
         var curr = performance.now();
-        if(curr - prev > delta) {
+        if(curr - prev > cfg.delta) {
             prev = curr;
-            step();
+            cfg.beat();
         }
-        requestAnimationFrame(loop);
+        requestAnimationFrame(function() {
+            loop(cfg);
+        });
     }
     function startLoop() {
         isLooping = true;
-        loop();
+        loop(loadConfig());
     }
     function stopLoop() {
         isLooping = false;
+    }
+    var _ = {times: function() {console.error('Not implemented');}}
+    function loadConfig() {
+        var config = {
+            bpm: 120,
+            signature: {
+                numerator: 4,
+                denominator: 4
+            }
+        };
+        function mkBeat(n) {
+            var tick   = new Audio('/metro/assets/tick.mp3'),
+                tock   = new Audio('/metro/assets/tock.mp3'),
+                curr   = 0,
+                sounds = [tick];
+            if(n > 1) {
+                var t = Array(n-1).fill(tock);
+                sounds = sounds.concat(t);
+            }
+            return function() {
+                var s = sounds[curr++ % sounds.length];
+                s.play();
+            }
+        }
+        return {
+            delta: 60 / config.bpm *1000,
+            beat: mkBeat(config.signature.numerator)
+        };
     }
     return {
         play: startLoop,
